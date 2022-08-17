3bdc35cc4ac43f219c8fec39957a55c344463359
Mostafa Mahmoud <mostafa447@hotmail.com>
2022-08-17 06:53:04 +0200

    addHomeCard

--- lib/modules/feeds/feeds_screen.dart
+++ lib/modules/feeds/feeds_screen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 
 class FeedsScreen extends StatelessWidget {
-  const FeedsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context)
   {
-    return Text(
-      'Home',
+    return Column(
+      children:
+      [
+        Card(
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          elevation: 20.0,
+          margin: EdgeInsets.all(8.0),
+          child: Stack(
+            children: [
+              Image.network(
+                'https://img.freepik.com/free-photo/group-friends-jumping-top-hill_273609-15304.jpg',
+                fit: BoxFit.cover,
+                height: 180.0,
+                width: double.infinity,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Communicate With Your Friends',
+                style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Colors.white,)),
+              ),
+            ]
+          ),
+        ),
+      ],
     );
   }
 }
